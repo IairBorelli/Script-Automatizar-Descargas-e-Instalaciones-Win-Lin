5889903cc5eb797da55129af171f1572804de745
Marcelo Iair Borelli <borelli.iair@gmail.com>
2024-05-27 12:44:06 -0300

    Add files via upload

--- install_win.py
+++ install_win.py
@@ -27,24 +27,24 @@ if not is_admin():
     sys.exit(0)
 
 # URLs de descarga
-download_urls = {
-    "Steam": "https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe",
-    "Brave Browser": "https://laptop-updates.brave.com/latest/winx64",
-    "Discord": "https://discord.com/api/download?platform=win",
-    "Telegram": "https://updates.tdesktop.com/tsetup/tsetup.exe",
-    "Audacity": "https://fossies.org/windows/misc/audacity-win-3.1.3-x64.exe",
-    "Visual Studio Code": "https://update.code.visualstudio.com/latest/win32-x64-user/stable",
-    "Git": "https://github.com/git-for-windows/git/releases/latest/download/Git-2.36.1-64-bit.exe",
-    "GitHub Desktop": "https://central.github.com/deployments/desktop/desktop/latest/win32",
-    "Cougar Minos XC Driver": "https://cougargaming.com/fileadmin/downloads/minos_x2_1.08.zip",
-    "TLauncher": "https://tlauncher.org/jar",
-    "Free Download Manager": "https://dn3.freedownloadmanager.org/6/latest/fdm_x64_setup.exe",
-    "WO Mic Client": "https://wolicheng.com/womic/files/WoMicClientSetup.exe",
-    "Python": "https://www.python.org/ftp/python/3.10.2/python-3.10.2-amd64.exe",
-    "Readmin VPN": "https://download.radmin.com/download/files/RVServer_3.5.2.1_EN.msi",
-    "Revo Uninstaller": "https://www.revouninstaller.com/download-professional-version.php",
-    "Wise Memory Optimizer": "https://www.wisecleaner.com/soft/WiseMemoryOptimizer.exe"
-}
+download_urls = [
+    "https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe",
+    "https://laptop-updates.brave.com/latest/winx64",
+    "https://discord.com/api/download?platform=win",
+    "https://updates.tdesktop.com/tsetup/tsetup.exe",
+    "https://fossies.org/windows/misc/audacity-win-3.1.3-x64.exe",
+    "https://update.code.visualstudio.com/latest/win32-x64-user/stable",
+    "https://github.com/git-for-windows/git/releases/latest/download/Git-2.36.1-64-bit.exe",
+    "https://central.github.com/deployments/desktop/desktop/latest/win32",
+    "https://cougargaming.com/fileadmin/downloads/minos_x2_1.08.zip",
+    "https://tlauncher.org/jar",
+    "https://dn3.freedownloadmanager.org/6/latest/fdm_x64_setup.exe",
+    "https://wolicheng.com/womic/files/WoMicClientSetup.exe",
+    "https://www.python.org/ftp/python/3.10.2/python-3.10.2-amd64.exe",
+    "https://download.radmin.com/download/files/RVServer_3.5.2.1_EN.msi",
+    "https://www.revouninstaller.com/download-professional-version.php",
+    "https://www.wisecleaner.com/soft/WiseMemoryOptimizer.exe"
+]
 
 # URL de descarga de Spotify
 spotify_url = "https://download.scdn.co/SpotifySetup.exe"
@@ -76,10 +76,10 @@ def run_installer(file_path):
         print(f"Failed to install: {os.path.basename(file_path)}")
 
 # Descargar e instalar cada programa
-for program, url in download_urls.items():
-    print(f"Downloading and installing: {program}")
-    file_name = url.split("/")[-1]
-    installer_path = download_file(url, file_name)
+for url in download_urls:
+    program_name = url.split("/")[-1]
+    print(f"Downloading and installing: {program_name}")
+    installer_path = download_file(url, program_name)
     if installer_path:
         run_installer(installer_path)
 
